b66cb459deb60582ce5bb12b7da2887ec5484f83
jianan1104 <linjianan1104@gmail.com>
2022-10-02 03:12:36 +0800

    refactor(app/settings.py): Change language and time zone

--- app/app/settings.py
+++ app/app/settings.py
@@ -113,9 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
