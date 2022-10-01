cbd503d4dff9dcbf193d91916e4eef829c8bdeb7
jianan1104 <linjianan1104@gmail.com>
2022-10-02 02:14:32 +0800

    build(settings.py): Add core app in INSTALLED_APPS

---
+++ app/core/test/test_command.py
@@ -0,0 +1,30 @@
+"""
+Test custom Django management commands.
+"""
+
+from unittest.mock import patch
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    ''' Test commands. '''
+
+    def test_wait_for_db_ready(self, patched_check):
+        ''' Test waiting for database if database ready. '''
+        patched_check.return_value = True
+        call_command('wait_for_db')
+        patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        ''' Test waiting for database when getting OperationalError. '''
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+        call_command('wait_for_db')
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
