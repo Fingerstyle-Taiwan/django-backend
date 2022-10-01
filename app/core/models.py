b9d21a4ee3be659ba86c87d5c0cd09cfaf2db3f4
jianan1104 <linjianan1104@gmail.com>
2022-10-02 03:16:18 +0800

    test(core/test/test_models.py): Add test for user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,52 @@
+'''
+Database models.
+'''
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    '''
+    Manager for users.
+    '''
+    def create_user(self, email, password=None, **extra_fields):
+        '''
+        Create and Save User
+        '''
+        if not email:
+            raise ValueError('User must have an email. ')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        ''' Create and return a new superuser. '''
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    '''
+    User Model
+    '''
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+
