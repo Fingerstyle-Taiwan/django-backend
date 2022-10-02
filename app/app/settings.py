c45b7941bb839696c83937c6d4f7c550c6d3076a
jianan1104 <linjianan1104@gmail.com>
2022-10-02 20:47:33 +0800

    feat(app/app/settings.py): Setup rest_framwork and swagger
    See APIs docs -> http://127.0.0.1
    
    BREAKING CHANGE:

--- app/app/settings.py
+++ app/app/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path(
+        'api/docs/',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs'
+    ),
 ]
