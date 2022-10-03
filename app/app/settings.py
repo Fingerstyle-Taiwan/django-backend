8f4548217ae87f46210eb79bafa98da31ce7862a
jianan1104 <linjianan1104@gmail.com>
2022-10-03 23:57:44 +0800

    build(app/app/settings.py): Add static/media root and url

--- app/app/settings.py
+++ app/app/settings.py
@@ -129,7 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
