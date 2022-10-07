4e46a993f61751fe02548f305ea0c10837018b47
jianan1104 <linjianan1104@gmail.com>
2022-10-07 20:31:42 +0800

    refactor: Finish profile model implement
    BREAKING CHANGE: Add profile model to user model

--- app/user/urls.py
+++ app/user/urls.py
@@ -10,5 +10,5 @@ app_name = 'user'
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
     path('token/', views.CreateTokenView.as_view(), name='token'),
-    path('me/', views.ManageUserView.as_view(), name='me')
+    path('me/', views.ManageUserView.as_view(), name='me'),
 ]
