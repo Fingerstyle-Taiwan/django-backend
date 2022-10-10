8c929dfa9ff1747492f5975f1f01cf1e66ce878b
jianan1104 <linjianan1104@gmail.com>
2022-10-11 00:26:06 +0800

    feat(artist/views.py): Implement artist views

--- app/artist/views.py
+++ app/artist/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+'''
+Views for artist API.
+'''
+from rest_framework import mixins
+from rest_framework.generics import GenericAPIView
+from artist.serializer import ArtistSerializer
+from core.models import Artist
 
-# Create your views here.
+
+class CreateListItems(mixins.ListModelMixin, GenericAPIView):
+
+    serializer_class = ArtistSerializer
+    queryset = Artist.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
