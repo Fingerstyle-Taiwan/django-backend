56f0bc39947472e514858c2866726b4d7ca0bba3
jianan1104 <linjianan1104@gmail.com>
2022-10-12 12:18:55 +0800

    refactor(app/urls.py): Edit API url name

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,12 +22,12 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path('schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
-        'api/docs/',
+        'docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs'
     ),
-    path('api/user/', include('user.urls')),
-    path('api/artist/', include('artist.urls'))
+    path('user/', include('user.urls')),
+    path('artist/', include('artist.urls'))
 ]
