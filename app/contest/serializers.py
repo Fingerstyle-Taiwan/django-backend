546b701c2e528fb2da0e2ed754745202a7236f6e
Benson Yang <f932037@gmail.com>
2022-10-13 23:09:26 +0800

    fix(app/contest/serializers.py): fix lint to serializers

--- app/contest/serializers.py
+++ app/contest/serializers.py
@@ -1,8 +1,8 @@
 from core.models import Contest
 from rest_framework import serializers
 
+
 class ContestSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Contest
         fields = '__all__'
