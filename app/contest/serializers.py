6c8412e03b786cc3ce2f45088f9d058c40e69c1d
Loume <chan.loume@gmail.com>
2022-11-03 12:22:27 +0800

    feat(contest/serializers.py): add comments serializer to contest model

--- app/contest/serializers.py
+++ app/contest/serializers.py
@@ -1,4 +1,4 @@
-from core.models import Contest
+from core.models import Contest, Comments
 from rest_framework import serializers
 from taggit.serializers import (TagListSerializerField,
                                 TaggitSerializer)
@@ -9,11 +9,23 @@ class ContestSerializer(TaggitSerializer, serializers.ModelSerializer):
         source='likes.count',
         read_only=True
     )
+    comment_count = serializers.IntegerField(
+        source='comments.count',
+        read_only=True
+    )
     tags = TagListSerializerField()
 
     class Meta:
         model = Contest
-        fields = ['id', 'name', 'organizer', 'like_count', 'tags']
+        fields = ['id', 'name', 'organizer', 'like_count', 'comment_count', 'tags']
+
+
+class ContestCommentSerializer(TaggitSerializer, serializers.ModelSerializer):
+    user_name = serializers.CharField(source='user.name')
+
+    class Meta:
+        model = Comments
+        fields = ['id', 'content', 'user_name', 'updated_at']
 
 
 class ContestDetailSerializer(TaggitSerializer, serializers.ModelSerializer):
@@ -22,9 +34,10 @@ class ContestDetailSerializer(TaggitSerializer, serializers.ModelSerializer):
         read_only=True
     )
     is_liked = serializers.BooleanField(read_only=True, default=False)
+    comments = ContestCommentSerializer(many=True)
     tags = TagListSerializerField()
 
     class Meta:
         model = Contest
         fields = '__all__'
-        extra_fields = ['like_count', 'is_liked', 'tags']
+        extra_fields = ['like_count', 'is_liked', 'comments', 'tags']
