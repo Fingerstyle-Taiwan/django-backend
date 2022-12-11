de02864409e4e8eb5b1658ee32037eae60377eb3
Kane <kane@KanedeMacBook-Pro.local>
2022-12-12 04:22:02 +0800

    refactor(app/contest/urls.py): Change contest list url

--- app/contest/urls.py
+++ app/contest/urls.py
@@ -4,7 +4,7 @@ from contest import views
 app_name = 'contest'
 
 urlpatterns = [
-    path('', views.ContestView.as_view(), name='contest-list'),
+    path('list/', views.ContestView.as_view(), name='contest-list'),
     path('<int:pk>/', views.ContestDetailView.as_view(),
          name='contest-detail'),
     path('<int:pk>/like/', views.ContestLikeView.as_view(),
